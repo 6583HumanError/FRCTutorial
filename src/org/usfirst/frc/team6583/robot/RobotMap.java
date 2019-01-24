226e981529e52871801f68a0947adfc6fe00b252
6583HumanError <nshrobotics@newschoolhigh.org>
2019-01-23 19:47:25 -0500

    Init Commit

---
+++ src/org/usfirst/frc/team6583/robot/RobotMap.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team6583.robot;
+
+
+public enum RobotMap {
+	
+	LEFT_MOTOR(0),
+	RIGHT_MOTOR(1);
+	
+	public final int value;
+	
+	RobotMap(int value) {
+		this.value = value;
+	
+	}
+	
+	
+	
+}
---
+++ src/org/usfirst/frc/team6583/robot/subsystems/Drivebase.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team6583.robot.subsystems;
+
+import org.usfirst.frc.team6583.robot.*;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.*;
+
+public class Drivebase extends Subsystem {
+	
+	private Spark LeftMotor;
+	private Spark RightMotor;
+
+	public Drivebase ( ) {
+		LeftMotor = new Spark(RobotMap.LEFT_MOTOR.value);
+		RightMotor = new Spark(RobotMap.RIGHT_MOTOR.value);
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
